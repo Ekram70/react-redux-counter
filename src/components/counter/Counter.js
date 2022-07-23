8db2069034da0c01247372fe8277663c05c58114
Ekram70 <ekramullah70@gmail.com>
2022-07-24 00:22:33 +0600

    set custom number

--- src/components/counter/Counter.js
+++ src/components/counter/Counter.js
@@ -1,9 +1,16 @@
+import cogoToast from 'cogo-toast';
+import { useRef } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
-import { decrement, increment } from '../../redux/state/counter/counterSlice';
+import {
+  decrement,
+  increment,
+  setCustom,
+} from '../../redux/state/counter/counterSlice';
 
 function Counter() {
-  const dispatch = useDispatch();
+  const number = useRef();
 
+  const dispatch = useDispatch();
   const count = useSelector((state) => state.counter.value);
 
   return (
@@ -26,6 +33,24 @@ function Counter() {
               className="btn btn-danger mx-2"
             >
               Decrease
+            </button>
+          </div>
+          <div className="my-4">
+            <input ref={number} className="form-control" type="number" />
+            <button
+              onClick={() => {
+                if (number.current.value === '') {
+                  cogoToast.error('Please enter a number');
+                } else if (number.current.value < 0) {
+                  cogoToast.error("Value can't be less than 0");
+                } else {
+                  dispatch(setCustom(number.current.value));
+                  number.current.value = '';
+                }
+              }}
+              className="btn my-4 btn-warning"
+            >
+              Set Custom
             </button>
           </div>
         </div>
--- src/redux/state/counter/counterSlice.js
+++ src/redux/state/counter/counterSlice.js
@@ -1,4 +1,5 @@
 import { createSlice } from '@reduxjs/toolkit';
+import cogoToast from 'cogo-toast';
 
 export const counterSlice = createSlice({
   name: 'counter',
@@ -10,10 +11,17 @@ export const counterSlice = createSlice({
       state.value += 1;
     },
     decrement: (state) => {
-      state.value -= 1;
+      if (state.value > 0) {
+        state.value -= 1;
+      } else {
+        cogoToast.error("Value can't be less than 0");
+      }
+    },
+    setCustom: (state, action) => {
+      state.value = +action.payload;
     },
   },
 });
 
-export const { increment, decrement } = counterSlice.actions;
+export const { increment, decrement, setCustom } = counterSlice.actions;
 export default counterSlice.reducer;
